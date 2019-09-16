3509ec1924c90b6eafc8f18277dba9f562497879
Alishaan Ali <alishaan64@gmail.com>
2019-09-16 17:00:21 -0400

    Beginnings of GUI

--- Triangle/src/com/company/TriangleTester.java
+++ Triangle/src/com/company/TriangleTester.java
@@ -1,8 +1,28 @@
 package com.company;
 
+//Imports
+//For Color
+import java.awt.*;
+//For gui
+import javax.swing.*;
+//For click input
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class TriangleTester {
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) { //main class
+        //Creating new gui for this project
+        JFrame gui = new JFrame();
+        //Creating a new panel to display the coordinate system on
+        JPanel coordinate = new JPanel();
+        //Creating a new panel to display the triangle data on
+        JPanel trianglePanel = new JPanel();
+        //Creating SplitPanel with the two Panels to split the view
+        JSplitPane splitPanel = new JSplitPane(SwingConstants.VERTICAL, coordinate, trianglePanel);
+        gui.add(splitPanel);
+        gui.setSize(1000, 1000);
+        gui.setVisible(true);
     }
 }
