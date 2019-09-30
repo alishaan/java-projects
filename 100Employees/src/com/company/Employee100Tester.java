11a825a7d374b000ebfafb0894c757538fd89518
Alishaan Ali <alishaan64@gmail.com>
2019-09-30 14:52:39 -0400

    Finished 100 Employees //TODO add dynamic lengths

--- 100Employees/src/com/company/Employee100Tester.java
+++ 100Employees/src/com/company/Employee100Tester.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import java.io.*;
 import java.util.Random;
 
 public class Employee100Tester {
 
-    public static void main(String[] args) {
-        String randomLastName;
-        String randomFirstName;
+    public static void main(String[] args) throws IOException {
+        String randomLastName = "Smith";
+        String randomFirstName = "John";
         int randomNumDependents;
         int randomHoursWorked;
         double randomDoubleRate;
@@ -15,13 +16,49 @@ public class Employee100Tester {
         String[] randomEmployeeArray = new String[100];
         int employeeCount = 0;
 
+        //Get the amount of last names
+        int linesLast = 0;
+        try {
+            LineNumberReader lastNamesCount = new LineNumberReader(new FileReader("src/LastNames.txt"));
+            while (lastNamesCount.readLine() != null) {
+                linesLast++;
+            }
+            lastNamesCount.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        //Get the amount of Male First Names
+        int linesMaleNames = 0;
+        try {
+            LineNumberReader maleNamesCount = new LineNumberReader(new FileReader("src/MaleNamesDictionary.txt"));
+            while (maleNamesCount.readLine() != null) {
+                linesMaleNames++;
+            }
+            maleNamesCount.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        //Get the amount of Female First Names
+        int linesFemaleNames = 0;
+        try {
+            LineNumberReader femaleNamesCount = new LineNumberReader(new FileReader("src/MaleNamesDictionary.txt"));
+            while (femaleNamesCount.readLine() != null) {
+                linesFemaleNames++;
+            }
+            femaleNamesCount.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        //Get the amount of Female Last Names
+        //Get the amount of Male Last Names
         do{
             employeeCount++;
             //Generate random values between certain
             //Dependents
             randomNumDependents = (int)(Math.random()*6);
             //Hours Worked
-            randomHoursWorked = (int)(Math.random()*80);
+            randomHoursWorked = (int)(Math.random()*60)+20;
             //Pay rate
             randomDoubleRate = (Math.random()*25)+1;
             //Age
@@ -33,14 +70,53 @@ public class Employee100Tester {
             } else {
                 randomSex = "female";
             }
-            randomFirstName = "alishaan";
-            randomLastName = "ali";
+            //Last Name
+            try {
+                LineNumberReader lastNamesReader = new LineNumberReader(new FileReader("src/LastNames.txt"));
+                int randomLastNameInt = (int)(linesLast * Math.random());
+                for(int i=0; i<randomLastNameInt; i++) {
+                    randomLastName = lastNamesReader.readLine();
+                }
+                lastNamesReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //First Name by Sex
+            if (randomSex.equals("male")){
+                try {
+                    LineNumberReader maleNamesReader = new LineNumberReader(new FileReader("src/MaleNamesDictionary.txt"));
+                    int randomMaleFirstNameInt = (int)(linesMaleNames * Math.random());
+                    for(int i=0; i<randomMaleFirstNameInt; i++) {
+                        randomFirstName = maleNamesReader.readLine();
+                    }
+                    maleNamesReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                try {
+                    LineNumberReader maleNamesReader = new LineNumberReader(new FileReader("src/FemaleNamesDictionary.txt"));
+                    int randomFemaleFirstNameInt = (int)(linesFemaleNames * Math.random());
+                    for(int i=0; i<randomFemaleFirstNameInt; i++) {
+                        randomFirstName = maleNamesReader.readLine();
+                    }
+                    maleNamesReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             RandomEmployee randomEmployee = new RandomEmployee(randomFirstName, randomLastName, randomHoursWorked, randomDoubleRate, randomAge, randomNumDependents, randomSex);
             randomEmployeeArray[employeeCount-1] = randomEmployee.toString();
 
         }while(employeeCount < 100);
+        //Make a large string
+        String randomEmployeeArrayString = "";
         for (int i=0; i < randomEmployeeArray.length; i++){
-            System.out.println(randomEmployeeArray[i]);
+            randomEmployeeArrayString = randomEmployeeArrayString +randomEmployeeArray[i] + System.lineSeparator();
         }
+        FileWriter write = new FileWriter("teacherOutput.txt");
+        PrintWriter print_line = new PrintWriter(write);
+        print_line.print(randomEmployeeArrayString);
+        print_line.close();
     }
 }
