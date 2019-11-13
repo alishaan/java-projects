a09dbfea3a577446624084d908a2dd5c0734bb6e
Alishaan Ali <alishaan64@gmail.com>
2019-11-13 08:11:47 -0500

    Finished matrix homework

---
+++ Matrix/src/com/company/Matrix.java
@@ -0,0 +1,78 @@
+package com.company;
+
+public class Matrix {
+    private int rows;
+    private int columns;
+    private double[][] matrix;
+
+    //creates matrix of 0's with a dimension
+    public Matrix (int r, int c){
+        rows = r;
+        columns = c;
+        matrix = new double[r][c];
+    }
+
+    //fill in by passing in a two dimensional array of values
+    public Matrix(double[][] data){
+        rows = data.length;
+        columns = data[0].length;
+        matrix = new double[rows][columns];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                matrix[i][j] = data[i][j];
+    }
+
+    //check if it can add or subtract
+    public boolean canAdd(Matrix toAdd){
+        if (toAdd.rows != this.rows || toAdd.columns != this.columns){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //Function to add matrices
+    public Matrix add(Matrix toAdd){
+        Matrix C = new Matrix(rows, columns);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                C.matrix[i][j] = this.matrix[i][j] + toAdd.matrix[i][j];
+        return C;
+    }
+
+    //toString Method
+    public String toString(){
+        String returnString = "";
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                returnString += Double.toString(matrix[i][j]) + " ";
+            }
+            returnString += System.lineSeparator();
+        }
+        returnString += System.lineSeparator();
+        return returnString;
+    }
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public double[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(double[][] matrix) {
+        this.matrix = matrix;
+    }
+}
---
+++ Matrix/src/com/company/MatrixTester.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class MatrixTester {
+
+    public static void main(String[] args) {
+        Matrix badMatrix = new Matrix(5, 5);
+        double[][] original = {{3,2,4,5}, {2, 1, 3, 5}, {3.3, 5, 2, 6}, {9, 5, 6, 7}};
+        Matrix startMatrix = new Matrix(original);
+
+        System.out.println(badMatrix.canAdd(startMatrix));
+
+        System.out.print(startMatrix.toString());
+
+        double[][] toCreate = {{1,2,4,5}, {9, 1, 3, 5}, {3.3, 5, 2, 6}, {12, 5, 6, 7}};
+        Matrix toAdd = new Matrix(toCreate);
+
+        System.out.print(toAdd.toString());
+
+        System.out.println(startMatrix.add(toAdd).toString());;
+
+    }
+}
