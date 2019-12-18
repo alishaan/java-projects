76f0593e06d6cd0534d087521ebad40ea2738879
Alishaan Ali <alishaan64@gmail.com>
2019-12-18 11:27:18 -0500

    Finished bookrec

--- BookRec/src/com/company/Book.java
+++ BookRec/src/com/company/Book.java
@@ -1,5 +1,4 @@
 package com.company;
-import javax.swing.*;
 import java.awt.*;
 
 public class Book{
--- BookRec/src/com/company/MyPanel.java
+++ BookRec/src/com/company/MyPanel.java
@@ -2,7 +2,6 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class MyPanel extends JPanel{
 
@@ -28,14 +27,14 @@ public class MyPanel extends JPanel{
         Book methC = myr.methodC(CurrentReader, myb);
         Book rec = myr.matchReader(CurrentReader, myb);
         g.setFont(new Font("Times Roman", Font.PLAIN, 50));
-        g.drawString("Popular", 10 , 50);
-        g.drawString("Method B", 250, 50);
-        g.drawString("Method C", 500, 50);
-        g.drawString("Custom", 900, 50);
-        pop.draw(g, 0, 100);
-        methB.draw(g, 250, 100);
-        methC.draw(g, 500, 100);
-        rec.draw(g, 900, 100);
+        g.drawString("Popular", 110 , 50);
+        g.drawString("Method B", 350, 50);
+        g.drawString("Method C", 600, 50);
+        g.drawString("Custom", 1000, 50);
+        pop.draw(g, 100, 100);
+        methB.draw(g, 350, 100);
+        methC.draw(g, 600, 100);
+        rec.draw(g, 1000, 100);
     }
 
 }
