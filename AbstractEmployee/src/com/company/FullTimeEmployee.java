a40c2fc01cc28a6e03c931c88181eea1adcb8e9f
Alishaan Ali <alishaan64@gmail.com>
2020-01-07 09:16:09 -0500

    Start Abstract Employee

---
+++ AbstractEmployee/src/com/company/FullTimeEmployee.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class FullTimeEmployee extends Employee {
+
+   public FullTimeEmployee(){
+      super();
+   }
+  
+   public double getPay() {
+      double pay;
+      
+      if (hours <= 40)
+         pay = rate * hours;
+      else
+         pay = rate * 40 +  rate * 2 * (hours - 40);
+
+      totalPay += pay;
+      return pay;
+   }
+}
+
---
+++ AbstractEmployee/src/com/company/PartTimeEmployee.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class PartTimeEmployee extends Employee {
+
+   public PartTimeEmployee() {
+      super();
+   }
+  
+   public double getPay() {
+      double pay;
+      pay = rate * hours;
+      totalPay += pay;
+      return pay;
+   }
+}
+
